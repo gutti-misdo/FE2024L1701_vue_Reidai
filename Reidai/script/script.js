4c72ccd6fc87aad3da500ec554454fe7b1b38e07
gutti-misdo <2301162@s.asojuku.ac.jp>
2024-11-19 00:44:11 +0900

    commit

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,16 +1,22 @@
 // JavaScript用ファイル
 new Vue({
-    el: '#app',
-    
-    data: {
+  el: '#app',
+
+  data: {
+    pass: '',
+    spass: ''
+  },
+  computed: {
+    isPasswordMatched() {
+      return this.pass === this.spass && this.pass !== '' && this.spass !== '';
     },
-    computed: {
-      // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
-      // 入力されているかは、this.･･･ で判定可能
-      // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
-      // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
 
-      // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
+    isButtonEnabled() {
+      return this.isPasswordMatched;
+    },
 
+    shouldShowMismatchMessage() {
+      return this.pass !== '' && this.spass !== '' && !this.isPasswordMatched;
     }
-  });
+  }
+});
